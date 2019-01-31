b8984e2aa7f21d00050d3200e9a9c08c06040b4b
ThandiTQ <ttshabalala@retrorabbit.co.za>
2019-01-31 22:54:22 +0200

    Scaffolded the db to create model
    Added Initial migration
    Created controllers
    Activated controllers by linking the db
    Tested the local api

---
+++ NARFO_BE/NARFO_BE/Models/Members.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NARFO_BE.Models
+{
+    public partial class Members
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- NARFO_BE/NARFO_BE/Startup.cs
+++ NARFO_BE/NARFO_BE/Startup.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NARFO_BE.Models;
 
-namespace BackendAPI
+namespace NARFO_BE
 {
     public class Startup
     {
@@ -25,6 +27,8 @@ namespace BackendAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<narfoContext>(opt =>
+                opt.UseInMemoryDatabase("narfo"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
