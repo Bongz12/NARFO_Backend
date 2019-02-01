d431744bbed20bcf23dc2ce0d11fb308ec3aa0f9
ThandiTQ <ttshabalala@retrorabbit.co.za>
2019-02-01 02:25:59 +0200

    Created connection to db
    Created a test to check the db connection
    Created read from db
    Created a test to check the output from reading from db

---
+++ NARFO_BE/ControllerUnitTests/ConnectionToDbTest.cs
@@ -0,0 +1,28 @@
+﻿using NARFO_BE;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControllerUnitTests
+{
+    public class ConnectionToDbTest
+    {
+        private ConnectionToDb connect;
+
+        public ConnectionToDbTest()
+        {
+            connect = new ConnectionToDb();
+        }
+
+        [TestCase]
+        public void DatabaseConnection()
+        {
+            //assign
+            var Opentest = connect.DBConnection();
+            //act
+            //assert
+            Assert.AreEqual("Open", Opentest);
+        }
+    }
+}
