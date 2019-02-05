a1ff6bbbb5904a12635e3a920299676b4229dbf9
Bongz12 <46738627+Bongz12@users.noreply.github.com>
2019-02-05 11:48:18 +0200

    hashing with sha256 with salt

--- NARFO_BE/NARFO_BE/encryption.cs
+++ NARFO_BE/NARFO_BE/encryption.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace NARFO_BE
 {
     public class encryption
-    {
+    {/*
         public static String sha256_hash(string value)
         {
             StringBuilder Password = new StringBuilder();
@@ -23,6 +23,70 @@ namespace NARFO_BE
             }
 
             return Password.ToString();
+        }*/
+        public static byte[] SaltBytes { get; private set; }
+
+       
+
+        public static string ComputeHash(string plainText, byte[] salt)
+        {
+            //var hash = 256;
+            int minSaltLength = 4, maxSaltLength = 16;
+
+            byte[] saltBytes = null;
+            if (salt != null)
+            {
+                saltBytes = salt;
+            }
+            else
+            {
+                Random r = new Random();
+                int SaltLength = r.Next(minSaltLength, maxSaltLength);
+                SaltBytes = new byte[SaltLength];
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                rng.GetNonZeroBytes(SaltBytes);
+                rng.Dispose();
+            }
+
+            byte[] plainData = ASCIIEncoding.UTF8.GetBytes(plainText);
+            byte[] plainDataWithSalt = new byte[plainData.Length + SaltBytes.Length];
+
+            for (int x = 0; x < plainData.Length; x++)
+                plainDataWithSalt[x] = plainData[x];
+            for (int n = 0; n < SaltBytes.Length; n++)
+                plainDataWithSalt[plainData.Length + n] = SaltBytes[n];
+
+            byte[] hashValue = null;
+
+          
+            SHA256Managed sha = new SHA256Managed();
+            hashValue = sha.ComputeHash(plainDataWithSalt);
+            sha.Dispose();
+                   
+
+            byte[] result = new byte[hashValue.Length + SaltBytes.Length];
+            for (int x = 0; x < hashValue.Length; x++)
+                result[x] = hashValue[x];
+            for (int n = 0; n < SaltBytes.Length; n++)
+                result[hashValue.Length + n] = SaltBytes[n];
+
+            return Convert.ToBase64String(result);
+        }
+
+        public static bool Confirm(string plainText, string hashValue)
+        {
+            
+            byte[] hashBytes = Convert.FromBase64String(hashValue);
+            int hashSize = 32;
+
+            byte[] saltBytes = new byte[hashBytes.Length - hashSize];
+
+            for (int x = 0; x < saltBytes.Length; x++)
+                saltBytes[x] = hashBytes[hashSize + x];
+
+            string newHash = ComputeHash(plainText, saltBytes);
+
+            return (hashValue == newHash);
         }
     }
 }
