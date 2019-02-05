8ba19905d1003de2a32a9fc87ccefc1a7d3edd1d
Bongz12 <46738627+Bongz12@users.noreply.github.com>
2019-02-05 11:11:31 +0200

    the encryption sha256_hash

---
+++ NARFO_BE/NARFO_BE/encryption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NARFO_BE
+{
+    public class encryption
+    {
+        public static String sha256_hash(string value)
+        {
+            StringBuilder Password = new StringBuilder();
+
+            using (var hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Password.Append(b.ToString("x2"));
+            }
+
+            return Password.ToString();
+        }
+    }
+}
